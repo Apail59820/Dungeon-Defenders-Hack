b8e2d188066e23cb696053c3022498a473b9cf7e
Apail59820 <92113742+Apail59820@users.noreply.github.com>
2024-04-28 00:29:04 +0200

    sdk upload

---
+++ SDK/DD_WinDrv_structs.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+// Dungeon Defenders (9.3.0) SDK
+
+#ifdef _MSC_VER
+	#pragma pack(push, 0x4)
+#endif
+
+#include "DD_Basic.hpp"
+#include "DD_Engine_classes.hpp"
+
+namespace Classes
+{
+}
+
+#ifdef _MSC_VER
+	#pragma pack(pop)
+#endif
